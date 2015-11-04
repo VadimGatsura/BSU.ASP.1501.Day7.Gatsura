c06e5924a2191af14a965b7a5af0bc261dd7da80
Vadim Gatsura <vadimgatsura@gmail.com>
2015-11-04 22:05:14 +0300

    Complete CountdownWatch task

--- Task1.Clock/CountdownWatch.cs
+++ Task1.Clock/CountdownWatch.cs
@@ -1,10 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Task1.Clock {
     public class CountdownWatch {
+        public event EventHandler FinishCountdown = delegate { };
+
+        private void OnFinishCountdown() {
+            var finishCountdown = FinishCountdown;
+            finishCountdown?.Invoke(this, new EventArgs());
+        }
+
+        public void StartCount(int milliseconds) {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            while (stopWatch.Elapsed.Milliseconds < milliseconds);
+            OnFinishCountdown();
+        }
     }
 }
---
+++ Task1.ConsoleUI/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using Task1.Clock;
+
+namespace Task1.ConsoleUI {
+
+    class Timer {
+        public void Register(CountdownWatch watch) {
+            if(watch == null)
+                throw new ArgumentNullException(nameof(watch));
+            watch.FinishCountdown += TimerCall;
+        }
+
+        public void UnRegister(CountdownWatch watch) {
+            if (watch == null)
+                throw new ArgumentNullException(nameof(watch));
+            watch.FinishCountdown -= TimerCall;
+        }
+
+        void TimerCall(object sender, EventArgs e) {
+            Console.WriteLine("Timer call");    
+        }
+    }
+
+    class Alarm {
+
+        public void Register(CountdownWatch watch) {
+            if (watch == null)
+                throw new ArgumentNullException(nameof(watch));
+            watch.FinishCountdown += AlarmCall;
+        }
+
+        public void UnRegister(CountdownWatch watch) {
+            if (watch == null)
+                throw new ArgumentNullException(nameof(watch));
+            watch.FinishCountdown -= AlarmCall;
+        }
+
+        void AlarmCall(object sender, EventArgs e) {
+            Console.WriteLine("Alarm call");
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            CountdownWatch watch = new CountdownWatch();
+            Timer timer = new Timer();
+            Alarm alarm = new Alarm();
+
+            Console.WriteLine("First call");
+            watch.StartCount(5);
+            Thread.Sleep(6);
+            timer.Register(watch);
+
+            Console.WriteLine("\nSecond call");
+            watch.StartCount(5);
+            Thread.Sleep(6);
+            alarm.Register(watch);
+
+            Console.WriteLine("\nThird call");
+            watch.StartCount(5);
+            Thread.Sleep(6);
+            timer.UnRegister(watch);
+
+            Console.WriteLine("\nFourth call");
+            watch.StartCount(5);
+            Thread.Sleep(6);
+            alarm.UnRegister(watch);
+
+            Console.WriteLine("\nFifth call");
+            watch.StartCount(5);
+            Thread.Sleep(6);
+            timer.UnRegister(watch);
+            alarm.Register(watch);
+
+            Console.WriteLine("\nSixth call");
+            watch.StartCount(5);
+            Thread.Sleep(6);
+            alarm.UnRegister(watch);
+            Console.ReadLine();
+        }
+    }
+}
