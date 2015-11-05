652ceaa1a0b0a46879c05226b0fc7ff44a52d6f5
Vadim Gatsura <vadimgatsura@gmail.com>
2015-11-05 13:26:20 +0300

    Complete BinarySearch task

---
+++ Task4.BinarySearch.NUnitTests/BinarySearchTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Task4.BinarySearch;
+
+namespace Task4.BinarySearch.NUnitTests {
+
+    public class ComparableComparer<T> : IComparer<T> where T: IComparable {
+        public int Compare(T x, T y) => x.CompareTo(y);
+    }
+
+    [TestFixture]
+    public class BinarySearchTest {
+        private IEnumerable<TestCaseData> TestDatas {
+            get {
+                yield return new TestCaseData(new[] { 15 }, 15, 0);
+                yield return new TestCaseData(new[] { 15 }, 12, -1);
+
+                yield return new TestCaseData(new[] { 12, 15 }, 12, 0);
+                yield return new TestCaseData(new[] { 12, 15 }, 15, 1);
+
+                yield return new TestCaseData(new[] { -58, -35, -28, -15, -1, 0, 2, 8, 12, 27, 35, 49, 59, 87, 98 }, 0, 5);
+                yield return new TestCaseData(new[] { -58, -35, -28, -15, -1, 0, 2, 8, 12, 27, 35, 49, 59, 87, 98 }, 98, 14);
+                yield return new TestCaseData(new[] { -58, -35, -28, -15, -1, 0, 2, 8, 12, 27, 35, 49, 59, 87, 98 }, -58, 0);
+
+                yield return new TestCaseData(new[] { -58, -1, 0, 0, 0, 2, 98 }, 0, 2);
+
+                yield return new TestCaseData(new[] { 0, 0, 0, 0, 0 }, 0, 0);
+                yield return new TestCaseData(new[] { -1, 0, 0, 1 }, 0, 1);
+                yield return new TestCaseData(new[] {-1, 0, 0, 0, 1}, 0, 1);
+
+                yield return new TestCaseData(new[] { 15, 12 }, 12, 1);
+                yield return new TestCaseData(new[] { 15, 12 }, 15, 0);
+                yield return new TestCaseData(new[] { 58, 35, 28, 15, 1, 0, -2, -8, -12, -27, -35, -49, -59, -87, -98 }, 1, 4);
+                yield return new TestCaseData(new[] { 58, 35, 28, 15, 1, 0, -2, -8, -12, -27, -35, -49, -59, -87, -98 }, -98, 14);
+                yield return new TestCaseData(new[] { 58, 35, 28, 15, 1, 0, -2, -8, -12, -27, -35, -49, -59, -87, -98 }, 58, 0);
+                yield return new TestCaseData(new[] { 58, 1, 0, 0, 0, -2, -98 }, 0, 2);
+                yield return new TestCaseData(new[] { 0, 0, 0, 0, 0 }, 0, 0);
+                yield return new TestCaseData(new[] { 1, 0, 0, -1 }, 0, 1);
+                yield return new TestCaseData(new[] { 1, 0, 0, 0, -1 }, 0, 1);
+
+                yield return new TestCaseData(new int[] { }, 15, -1).Throws(typeof(ArgumentException));
+            }
+        }
+        [Test, TestCaseSource(nameof(TestDatas))]
+        public void BinarySearch_Delegate_Test<T>(T[] array, T value, int result) {
+            Assert.AreEqual(array.Search(value, (a,b) => ((IComparable)a).CompareTo(b)), result);
+        }
+
+        [Test, TestCaseSource(nameof(TestDatas))]
+        public void BinarySearch_IComparer_Test<T>(T[] array, T value, int result) where T : IComparable {
+            Assert.AreEqual(array.Search(value, new ComparableComparer<T>()), result);
+        }
+    }
+}
--- Task4.BinarySearch/BinarySearch.cs
+++ Task4.BinarySearch/BinarySearch.cs
@@ -1,10 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Task3.BinarySearch {
-    public class BinarySearch {
+namespace Task4.BinarySearch {
+
+    public static class BinarySearch {
+
+        #region Public Methods
+
+        #region Delegate
+        public static int Search<T>(this T[] array, T value, int leftIndex, int rightIndex, Comparison<T> Compare) {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            if (array.Length == 0)
+                throw new ArgumentException("Array is empty");
+            if (leftIndex < 0 || leftIndex > array.Length)
+                throw new ArgumentOutOfRangeException(nameof(leftIndex));
+            if (leftIndex > rightIndex || rightIndex < 0 || rightIndex > array.Length)
+                throw new ArgumentOutOfRangeException(nameof(rightIndex));
+
+            if (array.Length == 1)
+                return Compare(array[0], value) == 0 ? 0 : -1;
+
+            int result = Compare(array[0], array[rightIndex]);
+            if (result == 0 && leftIndex < rightIndex)
+                return 0;
+            bool asc = result < 0;
+            if (asc && (Compare(value, array[0]) < 0 || Compare(value, array[rightIndex]) > 0))
+                return -1;
+            if (!asc && (Compare(value, array[0]) > 0 || Compare(value, array[rightIndex]) < 0))
+                return -1;
+
+            return Search(array, value, asc, leftIndex, rightIndex, Compare);
+        }
+        public static int Search<T>(this T[] array, T value, Comparison<T> Compare) => Search(array, value, 0, array.Length - 1, Compare);
+        #endregion
+
+        #region IComparer<T>
+        public static int Search<T>(this T[] array, T value, int leftIndex, int rightIndex, IComparer<T> comparator)
+            => Search(array, value, leftIndex, rightIndex, comparator.Compare);
+        public static int Search<T>(this T[] array, T value, IComparer<T> comparator) => Search(array, value, 0, array.Length - 1, comparator.Compare);
+        #endregion
+        #endregion
+
+        #region Private Methods
+        private static int Search<T>(T[] array, T value, bool asc, int left, int right, Comparison<T> Compare) {
+            if (left > right)
+                return -1;
+            if (left == right)
+                return Compare(array[left], value) == 0 ? left : -1;
+
+            int middle = left + (right - left) / 2;
+            if (Compare(value, array[middle]) == 0) {
+                return middle - left < 1 ? middle : Search(array, value, asc, left, middle, Compare);
+            }
+
+            if ((Compare(value, array[middle]) < 0 ^ !asc))//&& asc
+                return Search(array, value, asc, left, middle, Compare);
+            return Search(array, value, asc, middle + 1, right, Compare);
+        }
+        #endregion
     }
 }
